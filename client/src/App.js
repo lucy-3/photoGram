11ab8e9e2316afaee8544150c0e6887687fa523a
kaylijoi <kaylideshon@gmail.com>
2021-04-06 19:11:02 -0700

    Add react files - components and auth added

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Footer from './components/layout/Footer';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={ Landing } />
+          <Route exact path="/register" component={ Register } />
+          <Route exact path="/login" component={ Login } />
+          <Footer />
+        </div>
+      </Router>
+    )
+  }
+}
+
