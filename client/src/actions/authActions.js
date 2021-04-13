9fd0ec2cb20b59101da4cc2925cb4495d580bf3c
Lucy Wang <lucy.song.wang@gmail.com>
2021-04-12 20:08:06 -0700

    add actions and errorReducer

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,11 @@
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import axios from 'axios';
+
+export const registerUser = (userData) => dispatch => {
+  axios.post('/api/users/register', userData)
+    .then(res => console.log(res.data))
+    .catch(err => dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    }));
+}
